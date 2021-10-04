e9aac27bb2899d7b16ec031e99246bab166e5b0b
Andres Villalba <andresfelipevillalba486@gmail.com>
2021-10-04 18:56:45 -0500

    organizing-structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path='/results' component={Results} />
+				<Route path='/details:id' component={Details} />
+				<Redirect from='/' to='/results' />
+			</Switch>
+		</BrowserRouter>
+	)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(Root, document.getElementById('root'))
